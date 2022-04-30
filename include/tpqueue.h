0d88a14232480e131ff646933b351161c3d9486c
YablokovaValeriya <99179279+YablokovaValeriya@users.noreply.github.com>
2022-04-30 22:45:42 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -38,17 +38,17 @@ TPQueue<T>::~TPQueue() {
 template<typename T>
 void TPQueue<T>::push(const T& value) {
   if (tail && head) {
-    ITEM* first = head;
+    ITEM* tmp = head;
     ITEM* newItem = create(value);
     if ((head->value).prior < value.prior) {
       newItem->next = head;
       head = newItem;
     } else {
-      while (first->next != nullptr && (first->next->value.prior < value.prior)) {
-        first = first->next;
+      while (tmp->next != nullptr && (tmp->next->value.prior < value.prior)) {
+        tmp = tmp->next;
       }
-      newItem->next = first->next;
-      first->next = newItem;
+      newItem->next = tmp->next;
+      tmp->next = newItem;
     }
   } else {
     head = create(value);
